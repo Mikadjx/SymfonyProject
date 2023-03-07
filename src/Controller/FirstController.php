79c52e35bb97dd15c8cf978a546808b81b192c7c
mickael <chezdijoux@gmail.com>
2023-03-07 16:29:19 +0100

    page de connexion

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class FirstController extends AbstractController
 {
-    #[Route('/', name: 'app_acceuil')]
-    public function index(): Response
+    #[Route('/', name: 'app_home_page')]
+    public function index(EntityManagerInterface $entityManager,UserPasswordHasherInterface $userPasswordHasher ): Response
     {
+
+        
         return $this->render('homepage/index.html.twig', [
             
         ]);
